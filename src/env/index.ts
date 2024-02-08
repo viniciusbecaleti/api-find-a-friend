8c6bb7650f5f37171b0680b01c7270732ad01a4f
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-02-08 13:42:27 -0300

    feat: adiciona rota de cadastro de organização de forma simples

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.string().default('development'),
+  NODE_ENV: z.enum(['development', 'production']).default('development'),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
 })
