0135a7bff3a9bd4adbec239fa74926624e35fc68
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-04-27 12:33:17 -0300

    feat: adiciona a interface de resposta do serviço de pegar os detalhes de um pet

--- src/services/get-pet-details.service.ts
+++ src/services/get-pet-details.service.ts
@@ -1,14 +1,21 @@
 import { PetsRepository } from '@/repositories/pets.repository'
 import { PetNotFoundError } from './errors/pet-not-found-error'
+import { Pet } from '@prisma/client'
 
 interface GetPetDetailsServiceRequest {
   petId: string
 }
 
+interface GetPetDetailsServiceResponse {
+  pet: Pet
+}
+
 export class GetPetDetailsService {
   constructor(private petsRepository: PetsRepository) {}
 
-  async execute({ petId }: GetPetDetailsServiceRequest) {
+  async execute({
+    petId,
+  }: GetPetDetailsServiceRequest): Promise<GetPetDetailsServiceResponse> {
     const pet = await this.petsRepository.findById(petId)
 
     if (!pet) {
