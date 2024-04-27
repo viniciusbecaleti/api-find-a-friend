ebaf871b8675eab5dc43c75bac455d43f6be8945
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-04-27 12:30:48 -0300

    feat: adiciona findManyByOrganizationId no repositório de Pets

--- src/repositories/pets.repository.ts
+++ src/repositories/pets.repository.ts
@@ -3,4 +3,5 @@ import { Pet, Prisma } from '@prisma/client'
 export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   findById(id: string): Promise<Pet | null>
+  findManyByOrganizationId(organizationId: string): Promise<Pet[]>
 }
